ffbe552efc5557286ca4391705d071630d7d5d57
Turan Can Pamuk <turancan8726@outlook.com>
2023-04-12 13:38:04 +0300

    bugfix

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,9 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_LINK = process.env.MONGO_LINK || '';
 const MONGO_DBNAME = process.env.MONGO_DBNAME ? String(process.env.MONGO_DBNAME) : 'test';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@efes.qezhwwp.mongodb.net/${MONGO_DBNAME}?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_LINK}/${MONGO_DBNAME}?retryWrites=true&w=majority`;
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3002;
 
