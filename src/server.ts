946a6e8e0e7928d588926b704f83318388eef380
Turan Can Pamuk <turancan8726@outlook.com>
2023-04-18 20:13:57 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -72,7 +72,7 @@ const StartServer = () => {
         //     return res.status(statusCodes.Ok).json({});
         // }
         // next();
-        res.header('Access-Control-Allow-Origin', 'https://efes.tech'); // izinleri düzenle
+        res.header('Access-Control-Allow-Origin', 'https://efes.tech, ws://efes.tech'); // izinleri düzenle
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Credentials', 'true');
