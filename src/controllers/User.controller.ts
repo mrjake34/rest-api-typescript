a2bab6aa2656f7b416e9d87a14a592195cce5701
Turan Can Pamuk <turancan8726@outlook.com>
2023-04-19 14:38:12 +0300

    test update removed

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -129,13 +129,6 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
             domain: '.efes.tech',
             maxAge: 3600000 //1 hour
         });
-        res.cookie('client_session', accessToken, {
-            httpOnly: true,
-            secure: true,
-            // sameSite: 'none',
-            // domain: '.efes.tech',
-            maxAge: 3600000 //1 hour
-        });
 
         Logging.info(user.email + ' is logged in.', false);
         return res.status(statusCodes.Ok).json({ message: statusMessages.LoginSuccess, User: data }).end();
--- src/server.ts
+++ src/server.ts
@@ -72,7 +72,7 @@ const StartServer = () => {
         //     return res.status(statusCodes.Ok).json({});
         // }
         // next();
-        res.header('Access-Control-Allow-Origin', '*'); // izinleri düzenle
+        res.header('Access-Control-Allow-Origin', 'https://efes.tech'); // izinleri düzenle
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Credentials');
         res.header('Access-Control-Allow-Credentials', 'true');
         // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
