0efdf50d5230b5ded1d1aa4bf0c8534c95fb5d7f
Turan Can Pamuk <turancan8726@outlook.com>
2023-04-19 17:36:54 +0300

    Update order.controller.ts

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -122,7 +122,6 @@ export const updateOrder = async (req: RequestWithInterfaces, res: Response) => 
                     }
                 }
             }
-            console.log(orderId, updateOpts);
             const updatedOrder = await updateOrderFunction(orderId, updateOpts);
             Logging.info(statusMessages.UpdateSuccess, false);
             return res.status(statusCodes.Ok).json({ message: statusMessages.UpdateSuccess }).end();
