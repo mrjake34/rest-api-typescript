0fbceec70a87076fe4845c046def4e086d9f4b51
turancan-p <turancan8726@outlook.com>
2023-05-30 18:17:04 +0300

    header update

--- src/server.ts
+++ src/server.ts
@@ -59,7 +59,7 @@ const StartServer = () => {
     router.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', 'https://siparistakip.efes.tech');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Credentials, withCredentials, Authorization');
-        res.header(' ccess-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Credentials', 'true');
         next();
     });
 
